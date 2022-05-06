b8b5d0845ae8cd9183bab066b8e6463c1b833d46
Frank Bursitzke <frank.bursitzke@promx.net>
2022-05-06 13:32:33 +0200

    added --overwrite to deploy frontend

--- day5/apps/frontend/scmfe/public/settings/settings.js
+++ day5/apps/frontend/scmfe/public/settings/settings.js
@@ -1,8 +1,8 @@
 var uisettings = {
-    "endpoint": "",
-    "resourcesEndpoint": "",
-    "searchEndpoint": "",
-    "reportsEndpoint": "",
+    "endpoint": "https://app-contactsapi-devd5-l7vezsuz6dfps.azurewebsites.net",
+    "resourcesEndpoint": "https://app-resourcesapi-devd5-l7vezsuz6dfps.azurewebsites.net",
+    "searchEndpoint": "https://app-searchapi-devd5-l7vezsuz6dfps.azurewebsites.net",
+    "reportsEndpoint": "https://app-visitreportsapi-devd5-l7vezsuz6dfps.azurewebsites.net",
     "enableStats": false,
     "aiKey": "",
     "clientId": "",
